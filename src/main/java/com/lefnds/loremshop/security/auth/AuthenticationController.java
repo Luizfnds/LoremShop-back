412b8583286b7a8233f639b56538b38ea002d77c
Luiz Eduardo <98503542+Luizfnds@users.noreply.github.com>
2023-06-12 08:19:18 -0300

    Update AuthenticationController.java

--- src/main/java/com/lefnds/loremshop/security/auth/AuthenticationController.java
+++ src/main/java/com/lefnds/loremshop/security/auth/AuthenticationController.java
@@ -52,7 +52,7 @@ public class AuthenticationController {
                 //.secure(true)
                 .sameSite("Strict")
                 .path("/")
-                .domain(".vercel.app")
+                //.domain("lorem-shop-gules.vercel.app")
                 .maxAge(Duration.ofMillis(tokenExp - tokenIat))
                 .build();
 
